81db1c8e7d5ee817b79192cc8720bfc87621c3b1
Julio Vizarreta <julio.vizarreta@gmail.com>
2020-10-31 02:52:28 -0500

    Initial commit

---
+++ app/src/main/java/com/proyectofinal/homelife/fragmento3.java
@@ -0,0 +1,28 @@
+package com.proyectofinal.homelife;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragmento3 extends Fragment {
+
+    public fragmento3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragmento3, container, false);
+    }
+}
