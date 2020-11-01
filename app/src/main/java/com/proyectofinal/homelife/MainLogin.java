1a3f745efcaf501ac782b750d16bdaa412eb3c6b
Julio Vizarreta <julio.vizarreta@gmail.com>
2020-11-01 18:30:15 -0500

    6:30 PM
    Se eliminaron los fragmentos y se crearon Activity para Menu, Pago, RegistrarPago

--- app/src/main/java/com/proyectofinal/homelife/MainLogin.java
+++ app/src/main/java/com/proyectofinal/homelife/MainLogin.java
@@ -2,13 +2,38 @@ package com.proyectofinal.homelife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainLogin extends AppCompatActivity {
 
+    Button btnIniciarSesion;
+
+    public MainLogin() {
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_main_login, container, false);
+        btnIniciarSesion = v.findViewById(R.id.btnIniciarSesion);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainMenu.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        return v;
     }
 }
