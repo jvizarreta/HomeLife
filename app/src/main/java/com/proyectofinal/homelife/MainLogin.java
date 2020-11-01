0f7e6f8c0fa273f41ff0514625b109321ecfbaee
Claudia <cbolo.suasnabar@gmail.com>
2020-11-01 16:09:34 -0500

    Carga de menu y pago
    Trabaje sobre el fragmento menu

--- app/src/main/java/com/proyectofinal/homelife/MainLogin.java
+++ app/src/main/java/com/proyectofinal/homelife/MainLogin.java
@@ -2,13 +2,36 @@ package com.proyectofinal.homelife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainLogin extends AppCompatActivity {
+
+    Button btnIniciarS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
     }
+
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.activity_main_login, container, false);
+        btnIniciarS = v.findViewById(R.id.btnIniciarS);
+        btnIniciarS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), fragment_menu.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        return v;
+    }
+
 }
---
+++ app/src/main/java/com/proyectofinal/homelife/fragment_menu.java
@@ -0,0 +1,23 @@
+package com.proyectofinal.homelife;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class fragment_menu extends Fragment {
+
+
+
+
+    public fragment_menu() {
+
+    }
+
+
+}
