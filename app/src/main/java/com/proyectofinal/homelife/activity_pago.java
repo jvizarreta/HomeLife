c0097402d1801ababbc285a2e1cd6cad214a6ad9
Claudia <cbolo.suasnabar@gmail.com>
2020-11-01 17:44:07 -0500

    activity_pagos
    Se creó la actividad pagos

--- app/src/main/java/com/proyectofinal/homelife/activity_pago.java
+++ app/src/main/java/com/proyectofinal/homelife/activity_pago.java
@@ -1,28 +1,25 @@
 package com.proyectofinal.homelife;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
+
+
 import java.util.Calendar;
 
-
-public class fragment_Pago extends AppCompatActivity {
-
+public class activity_pago extends AppCompatActivity {
     EditText t1;
     private int mYearIni, mMonthIni, mDayIni, sYearIni, sMonthIni, sDayIni;
     static final int DATE_ID = 0;
     Calendar C = Calendar.getInstance();
-    protected void onCreate(Bundle savedInstanceState){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pago);
         sMonthIni = C.get(Calendar.MONTH);
         sDayIni = C.get(Calendar.DAY_OF_MONTH);
         sYearIni = C.get(Calendar.YEAR);
@@ -35,31 +32,38 @@ public class fragment_Pago extends AppCompatActivity {
                 showDialog(DATE_ID);
             }
         });
-
     }
 
-    private  void colocar_fecha(){
-        t1.setText((mMonthIni + 1) + "-" + mDayIni + "-" + mYearIni + "");
+    private void colocar_fecha() {
+        t1.setText((mMonthIni + 1) + "-" + mDayIni + "-" + mYearIni+" ");
     }
+
+
 
     private DatePickerDialog.OnDateSetListener mDateSetListener =
             new DatePickerDialog.OnDateSetListener() {
-
                 public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                     mYearIni = year;
                     mMonthIni = monthOfYear;
                     mDayIni = dayOfMonth;
                     colocar_fecha();
+
                 }
+
             };
 
-  protected Dialog onCreateDialog(int id) {
-      switch (id){
-          case DATE_ID:
-              return new DatePickerDialog(this, mDateSetListener, sYearIni, sMonthIni,sDayIni);
-      }
-      return null;
-  }
 
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_ID:
+                return new DatePickerDialog(this, mDateSetListener, sYearIni, sMonthIni, sDayIni);
+
+
+        }
+
+
+        return null;
+    }
 
 }
