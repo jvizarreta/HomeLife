a591997203c362695aa73b39d312cd77c572efd4
RCARRILLO123 <Rafael.Carrillo.fr@viaexperis.pe>
2020-11-01 19:06:14 -0500

    cambios angel

--- app/src/main/java/com/proyectofinal/homelife/EstadoCuenta.java
+++ app/src/main/java/com/proyectofinal/homelife/EstadoCuenta.java
@@ -1,4 +1,31 @@
 package com.proyectofinal.homelife;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class EstadoCuenta {
+public class EstadoCuenta extends Fragment {
+    BottomNavigationView menuBotones;
+    public EstadoCuenta(){
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_estado_cuenta, container, false);
+    }
 }
+
+
+
+
+
+
+
+
